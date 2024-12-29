3c5615e69bf260fce6b606f82d2868291e2ad55c
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-12-29 15:46:34 +0100

    J'ai termin la methode ajouterFilm()

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php 
+require './src/Admin.php';
+require './src/Film.php';
--- src/Admin.php
+++ src/Admin.php
@@ -1,8 +1,12 @@
 <?php
 require_once 'Persson.php';
+require_once 'Film.php';
 
 class Client extends Persson 
 {
-
+     public function ajouterFilm($titre,$genre,$durée,$dateSortie,$réalisateur,$distribution){
+        $id=rand(1,100);
+        return new Film($id,$titre,$genre,$durée,$dateSortie,$réalisateur,$distribution)
+     }
 
 }
